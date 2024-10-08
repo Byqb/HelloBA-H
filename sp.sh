f4ce039e8d049c331b8f1e4217fcd1e6b4f38fde
Yusuf <95586079+Byqb@users.noreply.github.com>
2024-10-08 22:28:04 +0300

    b

--- sp.sh
+++ sp.sh
@@ -7,3 +7,12 @@ Curent_Time="Getenrat on $TIME , Frome The $USER"
 
 echo "$TIME"
 echo "$Curent_Time"
+
+report_Time() {
+         echo "System Time : "
+
+        uptime
+#top = To See All The System
+}
+
+report_Time
