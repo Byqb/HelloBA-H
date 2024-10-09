a19028e3fe77cdc5141464aa2cfd6265877e7382
Yusuf <95586079+Byqb@users.noreply.github.com>
2024-10-09 17:16:50 +0300

    add More $cript Commands

--- sp.sh
+++ sp.sh
@@ -16,3 +16,38 @@ report_Time() {
 }
 
 report_Time
+
+#File Count
+file_Count=$(ls|wc -l)
+echo "Number of Files: $file_Count"
+
+#Disk Usage
+Disk=$(df -h)
+echo "The Disk Usage:"
+echo "$Disk"
+
+#System Info
+SystemInfo=$(uname -a)
+echo "The System Info : "
+echo "$SystemInfo"
+
+#User Info
+
+UserName=$(whoami)
+echo "The User is [$UserName]"
+
+#Process List
+
+ProscessList=$(ps aux)
+echo "Process List : "
+echo "$ProscessList"
+
+#Disk Space Alert
+threshold=90
+current_usage=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
+if [ "$current_usage" -ge "$threshold" ]; then
+    echo "Disk space is running low!"
+else
+    echo "Disk space is okay."
+fi
+
