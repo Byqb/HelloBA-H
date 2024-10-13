777cfa911028fbbf5eee58009eedda13aa4c5a99
Yusuf <95586079+Byqb@users.noreply.github.com>
2024-10-13 14:27:54 +0300

    Update sp.sh

--- sp.sh
+++ sp.sh
@@ -54,3 +54,60 @@ fi
 #Creat A Password
 Pass=$(tr -dc 'A-Za0-9!?%=' < /dev/urandom | head -c 10)
 echo "$Pass"
+
+#Var
+echo "Whats your First Name ?"
+read  F_N
+echo "Whats your Last Name ?"
+read L_N
+
+echo "Hello $F_N $L_N"
+
+#Positional arguments
+#Ex bash.sh $1=l $2=l
+echo Yo $1 $2
+
+
+#$howUpTime
+up="before"
+since="function"
+echo $up
+echo $since
+showuptime(){
+        local up=$(uptime -p | cut -c4-)
+        local since=$(uptime -s)
+        cat << E0F
+----
+This  Machine Has Been Up For ${up}
+It Has Been Running Since ${since}
+----
+E0F
+}
+showuptime
+echo $up
+echo $since
+
+#case Statement 
+clear
+echo "
+please select:
+
+1.Display System Info
+2.Display  HostName
+3.File List "
+
+read -p "Enter a Num [1-3] "
+
+case $REPLY in
+        1) echo "System Info : "
+        uptime
+;;
+
+        2) echo  "HostName $HOSTNAME"
+;;
+
+        3) echo "Files List :"
+        ls
+;;
+
+esac
